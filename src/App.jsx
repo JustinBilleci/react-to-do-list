4862470f9be2cae8c2f1816110a1184d0002a580
Justin Billeci <justinbilleci15@gmail.com>
2024-03-14 19:42:27 -0400

    finished my to-do list :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Todoinput from "./components/Todoinput"
 import Todolist from "./components/Todolist"
 
 function App() { // function name is always capitalixed just like the component file name(App.jsx)
   
-  const [todos, setTodos] = useState([]) // 'setTodos' is a stateful variable
+  const [todos, setTodos] = useState([])  // 'setTodos' is a stateful variable
+  const [todoValue, setTodoValue] = useState('')
+
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos:
+    newList }))
+  }
 
   function handleAddTodos(newTodo) {  // adding new todos
     const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -15,17 +22,34 @@ function App() { // function name is always capitalixed just like the component 
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index // if the todo index is not equal to the index then it removes it from the array
     })
+    persistData(newTodoList)
     setTodos(newTodoList) // cannot modify todos directly
   }
 
   function handleEditTodo(index) {
-
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
   }
+// if wanting to run upon page load, leave array empty
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
+      localTodos = JSON.parse(localTodos).todos
+      setTodos(localTodos)
+    
+  }, []) // since dependency array empty [] this will run after the page refreshes
 
   return ( // able to write javascript directly into the html
     <> 
-      <Todoinput handleAddTodos={handleAddTodos} /> 
-      <Todolist todos={todos} handleDeleteTodo={handleDeleteTodo}/>
+      <Todoinput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} /> 
+      <Todolist handleEditTodo={handleEditTodo} todos={todos} handleDeleteTodo={handleDeleteTodo}/>
     </> // <___ html={js} />
   )
 } // one functional component
--- src/components/Todoinput.jsx
+++ src/components/Todoinput.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react"
 
 export default function Todoinput(props) {
-    const {handleAddTodos} = props
-
-    const [todoValue, setTodoValue] = useState('')
+    const {handleAddTodos, todoValue, setTodoValue} = props
 
     return (
         <header>
